c419281843a28cec5c94238dc0aea2f8cc9ad3bf
willior <willior47@hotmail.com>
2019-01-24 18:15:57 -0500

    uploaded assignment 2

--- assignments/assignment2/js/Food.js
+++ assignments/assignment2/js/Food.js
@@ -10,10 +10,13 @@ class Food extends Agent {
   // Pass arguments on to the super() constructor (e.g. for Agent)
   // Also set a minimum and maximum size for this food object which it
   // will vary between when it resets
-  constructor(x,y,minSize,maxSize) {
+  constructor(x,y,minSize,maxSize,maxSpeed,tx,ty,vx,vy) {
     super(x,y,random(minSize,maxSize),'#55cccc');
     this.minSize = minSize;
     this.maxSize = maxSize;
+    this.maxSpeed = maxSpeed;
+    this.tx = random(0,100);
+    this.ty = random(0,100);
   }
 
   // reset()
@@ -24,5 +27,36 @@ class Food extends Agent {
     this.x = random(0,width);
     this.y = random(0,height);
     this.size = random(this.minSize,this.maxSize);
+    this.tx = random(0,100);
+    this.ty = random(0,100);
+
+  }
+
+  update() {
+
+    // movement
+    this.tx += 0.01;
+    this.vx = this.maxSpeed * (noise(this.tx) * 2 - 1);
+    this.ty += 0.01;
+    this.vy = this.maxSpeed * (noise(this.ty) * 2 - 1);
+    this.x += this.vx;
+    this.y += this.vy;
+
+    // wrapping
+    if (this.x < 0)  {
+      this.x += windowWidth;
+    }
+
+    if (this.x >= windowWidth) {
+      this.x -= windowWidth;
+    }
+
+    if (this.y < 0) {
+      this.y += windowHeight;
+    }
+
+    if (this.y >= windowHeight) {
+      this.y -= windowHeight;
+    }
   }
 }
--- assignments/assignment2/js/script.js
+++ assignments/assignment2/js/script.js
@@ -14,23 +14,23 @@ The player circle shrinks over time, but grows when it eats.
 
 // Constants for key quantities
 const AVATAR_MAX_SIZE = 64;
-const AVATAR_SIZE_LOSS_PER_FRAME = 1;
-const FOOD_MIN_SIZE = 5;
+const AVATAR_SIZE_LOSS_PER_FRAME = 0.4;
+const FOOD_MIN_SIZE = 20;
 const FOOD_MAX_SIZE = 100;
+
+const FOOD_MAX_SPEED = 16;
+
+// variables for food movement
+let FOOD_TEMP_X = 0.0;
+let FOOD_TEMP_Y = 0.0;
+let FOOD_VEL_X = 0;
+let FOOD_VEL_Y = 0;
 
 // Variables to store the two key objects
 let avatar;
-let food;
 
-
-// preload()
-//
-// Not needed
-
-function preload() {
-
-}
-
+// foods array
+let foods = [];
 
 // setup()
 //
@@ -39,8 +39,14 @@ function preload() {
 function setup() {
   createCanvas(windowWidth,windowHeight);
   avatar = new Avatar(mouseX,mouseY,AVATAR_MAX_SIZE,AVATAR_SIZE_LOSS_PER_FRAME)
-  food = new Food(random(0,width),random(0,height),FOOD_MIN_SIZE,FOOD_MAX_SIZE);
+
+// generating 32 foods
+  for (let i = 0; i < 32; i++) {
+    foods.push(new Food(random(0,width),random(0,height),FOOD_MIN_SIZE,FOOD_MAX_SIZE,FOOD_MAX_SPEED,FOOD_TEMP_X,FOOD_TEMP_Y,FOOD_VEL_X,FOOD_VEL_Y));
+  }
+  // food = new Food(random(0,width),random(0,height),FOOD_MIN_SIZE,FOOD_MAX_SIZE,FOOD_MAX_SPEED,FOOD_TEMP_X,FOOD_TEMP_Y,FOOD_VEL_X,FOOD_VEL_Y);
   noCursor();
+
 }
 
 
@@ -54,9 +60,18 @@ function draw() {
   background(0);
 
   avatar.update();
-  if (avatar.collide(food)) {
-    avatar.eat(food);
-  }
   avatar.display();
-  food.display();
+
+
+for (let i = 0; i < foods.length; i++) {
+  if (avatar.collide(foods[i])) {
+    avatar.eat(foods[i]);
+  }
+
+  foods[i].update();
+  foods[i].display();
+}
+
+  // food.update();
+  // food.display();
 }
