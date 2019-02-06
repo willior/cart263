9f6eecfb568f1eeb6a97e95bf5e6c9dfd7b27892
willior <willior47@hotmail.com>
2019-02-05 20:38:08 -0500

    fatigueLevel implementation; added fatigued (boolean)
    -made the fatigueLevel value factor into how OFTEN the rock slips when a button is pressed (fatigueLevel/100).
    -added boolean "fatigued" (true when fatigueLevel >= 50) which doubles (20 to 40) the range of distance the rock can slip.

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -9,7 +9,10 @@ let rockDistance;
 let powerLevel;
 let powerLevelDrain;
 let fatigueLevel;
-let fatigue;
+let fatigued;
+let slipDistance;
+let log;
+let slip;
 
 $(document).ready(setup);
 
@@ -19,13 +22,18 @@ function setup() {
   powerLevelDrain = 1;
   fatigueLevel = 0;
   fatigued = false;
+  slipDistance = 0;
+  log = " ";
+  slip = " ";
   $spans = $('span');
   $("#rockDistance").text(rockDistance);
   $("#powerLevel").text(powerLevel);
   $("#fatigueLevel").text(fatigueLevel);
+  $("#log").text(log);
+  $("#slip").text(slip);
   setInterval(update,100);
   setInterval(powerDrain,1000);
-  setInterval(rockSlip,1000);
+  setInterval(rockSlip,5000);
   setInterval(fatigueUpdate,500);
   $('span.push').on('click',pushClick);
   $('span.focus').on('click',powerClick);
@@ -33,11 +41,28 @@ function setup() {
 
 function pushClick() {
   rockDistance += powerLevel;
+  fatigueLevel++;
+  let p = Math.floor(Math.random() * (100-1)+1);
+  if (p < fatigueLevel) {
+    rockSlip();
+    // $("#log").fadeIn(1);
+    // $("#log").fadeOut(1000,textReset());
+    log = "\n the rock slipped " + slipDistance + " centimeters due to fatigue." + log;
+    console.log(slipDistance);
+  }
 }
 
 function powerClick() {
   powerLevel++;
   fatigueLevel++;
+  let f = Math.floor(Math.random() * (100-1)+1);
+  if (f < fatigueLevel) {
+    rockSlip();
+    // $("#log").fadeIn(1);
+    // $("#log").fadeOut(1000,textReset());
+    log = "\n the rock slipped " + slipDistance + " centimeters due to fatigue." + log;
+    console.log(slipDistance);
+  }
 }
 
 function powerDrain() {
@@ -45,8 +70,12 @@ function powerDrain() {
 }
 
 function rockSlip() {
-  let r = Math.random() * (10-1)+1;
-  rockDistance -= Math.floor(r);
+  if (fatigued = true) {
+    slipDistance = Math.floor(Math.random() * (40-1)+1);
+  } else if (fatigued = false) {
+    slipDistance = Math.floor(Math.random() * (20-1)+1);
+  }
+  rockDistance -= slipDistance;
 }
 
 function fatigueUpdate() {
@@ -54,12 +83,25 @@ function fatigueUpdate() {
   fatigueLevel = Math.min(100, Math.max(0, fatigueLevel));
 }
 
+function textReset() {
+  // don't know how to properly reset text after fadeOut
+  // $("#log").fadeIn(1);
+  log = " ";
+}
+
 function update() {
   if (rockDistance < 0) {
     rockDistance = 0;
   }
 
+  if (fatigueLevel >= 50) {
+    fatigued = true;
+  } else {
+    fatigued = false;
+  }
+
   $("#rockDistance").text(rockDistance);
   $("#powerLevel").text(powerLevel);
   $("#fatigueLevel").text(fatigueLevel);
+  $("#log").text(log);
 }
