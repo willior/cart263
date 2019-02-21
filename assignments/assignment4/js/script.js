c067f65505b24267b04a27cd1654cc960f0e0931
willior <willior47@hotmail.com>
2019-02-21 18:06:22 -0500

    updated sound effects and sound effect chooser
    1 of 4 sound effects are played when fruit is fed to Gene

--- assignments/assignment4/js/script.js
+++ assignments/assignment4/js/script.js
@@ -17,9 +17,16 @@ Chewing: https://freesound.org/people/InspectorJ/sounds/412068/
 // Sound effects for the experience
 let bgm1 = new Audio("assets/sounds/theme1.mp3");
 let bgm2 = new Audio("assets/sounds/theme2.mp3");
+
 let whistleSFX = new Audio("assets/sounds/Whistle.wav");
-let pain1 = new Audio("assets/sounds/Pain.wav");
-let pain2 = new Audio("assets/sounds/Pain 2.wav");
+let eat1SFX = new Audio("assets/sounds/Awesome!.wav");
+let eat2SFX = new Audio("assets/sounds/Great!.wav");
+let eat3SFX = new Audio("assets/sounds/I love it!.wav");
+let eat4SFX = new Audio("assets/sounds/Wow!.wav");
+
+
+let pain1SFX = new Audio("assets/sounds/Pain.wav");
+let pain2SFX = new Audio("assets/sounds/Pain 2.wav");
 
 // element variables
 let $gene;
@@ -29,6 +36,8 @@ let $cherry;
 
 // counter
 let eatCount;
+// variable used to randomly select an eat sound
+let random;
 
 $(document).ready(setup);
 
@@ -43,18 +52,15 @@ function setup() {
     drop: dropped
   });
 
-  // Get the page elements
-  $fly = $('#fly').draggable({
-    revert: "valid"
-  }).on("mousedown", function() {
-    pain1.play();
+  // page elements
+  // reverts fly's position when trying to feed to Gene
+  // has Gene utter disgustingly when fly is picked up
+  $fly = $('#fly').draggable({revert: "valid"}).on("mousedown", function() {
+    pain1SFX.play();
   });
-  $strawberry = $('#strawberry').draggable({
-
-  });
-  $cherry = $('#cherry').draggable({
-
-  });
+  // Gene whistles in anticipation of delicious fruit
+  $strawberry = $('#strawberry').draggable().on("mousedown", function() {whistleSFX.play()});
+  $cherry = $('#cherry').draggable().on("mousedown", function() {whistleSFX.play()});
 
   // Start up the music
   bgm1.loop = true;
@@ -66,14 +72,22 @@ function dropped (event,ui) {
   // if the fly is dropped on Gene, he utters more disgustingly
   // then returns so that the rest of the function does not run
   if(event.toElement === document.getElementById('fly')){
-    pain2.play();
+    pain2SFX.play();
     return;
   }
 
-  // runs if anything but the fly was fed to Gene
+  // runs if fruit was fed to Gene
   ui.draggable.remove();
   $(this).attr('src','assets/images/Gene2.png');
-  whistleSFX.play();
+
+  // picks a sound to play when Gene eats a fruit
+  random = Math.floor(Math.random() * (4 - 1)) + 1;
+  if (random === 1) {eat1SFX.play()}
+  if (random === 2) {eat2SFX.play()}
+  if (random === 3) {eat3SFX.play()}
+  if (random === 4) {eat4SFX.play()}
+
+  // keeps track of how much fruit Gene has consumed
   eatCount++;
   console.log(eatCount);
   if (eatCount < 4) {
