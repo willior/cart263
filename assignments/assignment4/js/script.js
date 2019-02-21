0076d10abc7af72c639342af91bd4721784d504a
willior <willior47@hotmail.com>
2019-02-21 17:48:37 -0500

    changed aesthetic to God Hand; reject works properly

--- assignments/assignment4/js/script.js
+++ assignments/assignment4/js/script.js
@@ -15,58 +15,73 @@ Chewing: https://freesound.org/people/InspectorJ/sounds/412068/
 ******************/
 
 // Sound effects for the experience
-let buzzSFX = new Audio("assets/sounds/buzz.mp3");
-let crunchSFX = new Audio("assets/sounds/crunch.wav");
+let bgm1 = new Audio("assets/sounds/theme1.mp3");
+let bgm2 = new Audio("assets/sounds/theme2.mp3");
+let whistleSFX = new Audio("assets/sounds/Whistle.wav");
+let pain1 = new Audio("assets/sounds/Pain.wav");
+let pain2 = new Audio("assets/sounds/Pain 2.wav");
 
-// Variable to hold our two key elements
-let $mouth;
+// element variables
+let $gene;
 let $fly;
 let $strawberry;
+let $cherry;
+
+// counter
+let eatCount;
 
 $(document).ready(setup);
 
 function setup() {
-  // Get the mouth element from the page
-  $mouth = $('#mouth');
+  eatCount = 0;
+  // Get the Gene element from the page
+  $gene = $('#gene');
   // Make it droppable
-  $mouth.droppable({
+  $gene.droppable({
     // The drop option specifies a function to call when a drop is completed
-    accept: "#strawberry",
-    drop: flyDropped
+    accept: "#strawberry, #cherry, #fly",
+    drop: dropped
   });
 
-  // Get the fly element from the page
-  $fly = $('#fly').draggable({ revert: "valid"});
-  // draggable
+  // Get the page elements
+  $fly = $('#fly').draggable({
+    revert: "valid"
+  }).on("mousedown", function() {
+    pain1.play();
+  });
+  $strawberry = $('#strawberry').draggable({
 
-  $strawberry = $('#strawberry').draggable();
+  });
+  $cherry = $('#cherry').draggable({
 
-  // Start up the buzzing of the fly
-  buzzSFX.loop = true;
-  buzzSFX.play();
+  });
+
+  // Start up the music
+  bgm1.loop = true;
+  bgm1.play();
 }
 
-// flyDropped(event,ui)
-//
-// Called when a draggable element is dragged over the droppable element (the mouth)
-// In this instance it can only be the fly (it's the only draggable element).
-// The arguments 'event' and 'ui' are automatically passed by jQuery UI and contain
-// helpful information about the event.
-function flyDropped (event,ui) {
-  // When we drop the fly into the mouth we should remove the fly from the page
-  // ui contains a reference to the draggable element that was just dropped in ui.draggable
-  // .remove() removes the select element from the page
-  ui.draggable.remove(); // $fly.remove() would work here too
-  // We should "close the mouth" by changing its image
-  // .attr() lets us change specific attributes on HTML element by specifying the attribute
-  // and then what we want to set it to - in this case the 'src' attribute to the closed image
-  $(this).attr('src','assets/images/mouth-closed.png');
-  // Now the fly is gone we should stop its buzzing
-  buzzSFX.pause();
-  // And start the crunching sound effect of chewing
-  crunchSFX.play();
-  // Use a setInterval to call the chew() function over and over
-  setInterval(chew,250);
+function dropped (event,ui) {
+
+  // if the fly is dropped on Gene, he utters more disgustingly
+  // then returns so that the rest of the function does not run
+  if(event.toElement === document.getElementById('fly')){
+    pain2.play();
+    return;
+  }
+
+  // runs if anything but the fly was fed to Gene
+  ui.draggable.remove();
+  $(this).attr('src','assets/images/Gene2.png');
+  whistleSFX.play();
+  eatCount++;
+  console.log(eatCount);
+  if (eatCount < 4) {
+    return;
+  }
+  else {
+    setInterval(chew,250);
+  }
 }
 
 // chew()
@@ -75,15 +90,16 @@ function flyDropped (event,ui) {
 function chew () {
   // We can use .attr() to check the value of an attribute to
   // In this case we check if the image is the open mouth
-  if ($mouth.attr('src') === 'assets/images/mouth-open.png') {
+  if ($gene.attr('src') === 'assets/images/Gene.png') {
     // If it is, we set the 'src' attribute to the closed mouth
-    $mouth.attr('src','assets/images/mouth-closed.png');
+    $gene.attr('src','assets/images/Gene2.png');
     // and play the crunching
-    crunchSFX.play();
+    bgm1.pause();
+    bgm2.play();
   }
   else {
     // Otherwise the 'src' attribute must have been the closed mouth
     // so we swap it for the open mouth
-    $mouth.attr('src','assets/images/mouth-open.png');
+    $gene.attr('src','assets/images/Gene.png');
   }
 }
