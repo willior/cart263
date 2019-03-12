ace249d78f3509169b973f2231ac6e3ecb08efbd
willjeez <willgs@gmail.com>
2019-03-12 12:41:50 -0400

    added commands for upgrading processor/expanding memory

--- projects/project2/js/script.js
+++ projects/project2/js/script.js
@@ -12,33 +12,75 @@ let memory;
 let cash;
 let traceLevel;
 
+let procPrice;
+let memoryPrice;
+
 window.addEventListener('load', setup);
 
 function setup() {
-  canvas = createCanvas(600, 200);
-  canvas.parent("mountain");
-  background(153);
+
   $spans = $('span');
+
+  procPower = 0;
+  memory = 0;
+  cash = 1000;
+  traceLevel = 0;
+  procPrice = 100;
+  memoryPrice = 100;
+
   $("#procPower").text(procPower);
   $("#memory").text(memory);
   $("#cash").text(cash);
   $("#traceLevel").text(traceLevel);
   $("#textLog").text(textLog);
+  $("#procPrice").text(procPrice);
+  $("#memoryPrice").text(memoryPrice);
+
   $('span.action').on('click',actionClick);
   $('span.upProcPower').on('click',procPowerUpgrade);
   $('span.upMemory').on('click',memoryUpgrade);
 }
 
 function actionClick() {
+  console.log("action!");
 
 }
 
 function procPowerUpgrade() {
 
+  console.log("processor upgrade");
+
+  if (cash < procPrice) {
+    textLog = "Not enough cash to upgrade processor.";
+    update();
+    return;
+  }
+  else {
+    procPower += 100;
+    cash -= procPrice;
+    procPrice = procPrice * 2;
+    textLog = "Processing power upgraded."
+    update();
+  }
 }
 
 function memoryUpgrade() {
 
+  console.log("memory upgrade");
+
+  if (cash < memoryPrice) {
+  textLog = "Not enough cash to upgrade memory.";
+  update();
+  return;
+  }
+
+  else {
+    memory += 4;
+    cash -= memoryPrice;
+    memoryPrice = memoryPrice * 2;
+    textLog = "Memory expanded."
+    update();
+  }
 }
 
 function update() {
@@ -47,4 +89,6 @@ function update() {
   $("#cash").text(cash);
   $("#traceLevel").text(traceLevel);
   $("#textLog").text(textLog);
+  $("#procPrice").text(procPrice);
+  $("#memoryPrice").text(memoryPrice);
 }
