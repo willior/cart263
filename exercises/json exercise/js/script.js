90d888f1e5052fa39a99ed1e94b7fea2c9cd08b5
willjeez <willgs@gmail.com>
2019-03-14 14:46:33 -0400

    completed condiments JSON exercise

--- exercises/json exercise/js/script.js
+++ exercises/json exercise/js/script.js
@@ -7,15 +7,28 @@ example of JSON syntax
 ******************/
 
 $(document).ready(function () {
-
-  $.getJSON('data/bestsellers.JSON',dataLoaded);
-  let wrong = 'wrong!';
-  let template = `This is a normal string, ${wrong}`;
-  console.log(template);
-
+  $.getJSON('data/data.JSON',dataLoaded);
 });
 
 function dataLoaded(data) {
-  let firstBook = data.books[0];
-  console.log(data);
+
+  let condiment = getRandomElement(data.condiments);
+  let cat = getRandomElement(data.cats);
+  let room = getRandomElement(data.rooms);
+  console.log(condiment);
+  let verb = 'is';
+  if (condiment.charAt(condiment.length - 1) === 's') {
+    verb = 'are';
+  }
+  console.log(verb);
+  console.log(cat);
+  console.log(room);
+  let description = `${condiment} ${verb} like a ${cat} in a ${room}`;
+  console.log(description);
+  $('body').append(description)
+}
+
+function getRandomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+
 }
