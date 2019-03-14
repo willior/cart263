0406adcb4847f3198cf128533288e82cd84c8908
willjeez <willgs@gmail.com>
2019-03-14 15:18:15 -0400

    started assignment 6; a/an works

--- assignments/assignment6/js/script.js
+++ assignments/assignment6/js/script.js
@@ -17,15 +17,21 @@ function dataLoaded(data) {
   let condiment = getRandomElement(data.condiments);
   let cat = getRandomElement(data.cats);
   let room = getRandomElement(data.rooms);
-  console.log(condiment);
   let verb = 'is';
   if (condiment.charAt(condiment.length - 1) === 's') {
     verb = 'are';
   }
+  let indefiniteArticle = 'a';
+  for (let i = 0; i < vowels.length; i++){
+    if (cat.toLowerCase().charAt(0) === vowels[i]) {
+      indefiniteArticle = 'an';
+    }
+  }
+    console.log(condiment);
   console.log(verb);
   console.log(cat);
   console.log(room);
-  let description = `${condiment} ${verb} like a ${cat} in a ${room}`;
+  let description = `${condiment} ${verb} like ${indefiniteArticle} ${cat} in a ${room}`;
   console.log(description);
   $('body').append(description)
 }
