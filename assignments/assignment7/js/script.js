a2a7eba00c2d8ade0cadcd235626d58da239de86
willjeez <willgs@gmail.com>
2019-03-28 15:56:28 -0400

    working on assignment 7
    drum loop, bass synth, lead synth, and background synth, all with different effects
    the lead synth has a makeshift low frequency oscillator modulating its lowpass filter's cutoff frequency

--- assignments/assignment7/js/script.js
+++ assignments/assignment7/js/script.js
@@ -2,63 +2,141 @@
 
 let bassFreqs = [55, 110];
 let synthFreqs = [440.00, 493.88, 523.25, 587.33, 659.25, 698.46, 783.99, 880.00];
+let synthFreqsB = [880.00, 1046.50, 1318.5, 1567.98];
 let pattern = [
 
-'k',
-'h',
-'s',
-'h',
+'kh',
+'x',
+'sh',
+'x',
 
 'h',
-'h',
-'s',
+'x',
+'sh',
 'k',
 
 'h',
 'k',
-'s',
-'h',
+'sh',
+'x',
 
 'h',
-'h',
-'s',
-'h',];
+'x',
+'sh',
+'k',
+
+];
 
 let patternIndex = 0;
-let bass, synth, kick, snare, hat;
+let bass, synth, synthB, kick, snare, hat;
 
-let dubDelay;
-let reverb;
-let reverbMod = 0.5;
-let reverbModUp = false;
+let drumDist;
+let drumLPF;
+let bassDist;
+let snareReverb;
+let synthLPF;
+let synthFlanger;
+let synthReverb;
+let synthBdelay;
+
+let lowpassLFO = 400;
+let lowpassLFOup = true;
 
 let playing = false;
 
-let noteRNG;
+let lengthRNG;
+let restRNG;
 
 function preload() {
 
 }
 
 function setup() {
-  bass = new Pizzicato.Sound({
-    source: 'wave',
-  })
-  synth = new Pizzicato.Sound({
-    source: 'wave',
-  });
-
+  // percussion
   kick = new Pizzicato.Sound('/assets/sounds/kick.wav');
   snare = new Pizzicato.Sound('/assets/sounds/snare.wav');
   hat = new Pizzicato.Sound('/assets/sounds/hihat.wav');
+  kick.release = 0;
+  snare.release = 0;
+  hat.release = 0;
 
-  reverb = new Pizzicato.Effects.Reverb({
-    time: 0.5,
-    decay: 0.5,
-    reverse: false,
-    mix: reverbMod
+  // creating a group for the percussion
+  var drums = new Pizzicato.Group();
+  drums.addSound(kick)
+  drums.addSound(snare)
+  drums.addSound(hat)
+  // percussion effects
+  drumDist = new Pizzicato.Effects.Distortion({
+    gain: 1
   });
-  synth.addEffect(reverb);
+  drumLPF = new Pizzicato.Effects.LowPassFilter({
+    frequency: 2400,
+    peak: 16
+  });
+  snareReverb = new Pizzicato.Effects.Reverb({
+    time: 0.5,
+    decay: 1,
+    reverse: false,
+    mix: 0.5
+  });
+
+  // bass synth
+  bass = new Pizzicato.Sound({
+    source: 'wave',
+  })
+  // bass synth effects
+  bassDist = new Pizzicato.Effects.Distortion({
+    gain: 0.5
+  });
+
+  // lead synth
+  synth = new Pizzicato.Sound({
+    source: 'wave',
+    options: {
+      type: 'triangle'
+    }
+  })
+
+  // lead synth effects
+  synthLPF = new Pizzicato.Effects.LowPassFilter({
+    frequency: lowpassLFO,
+    peak: 16
+  })
+
+  synthFlanger = new Pizzicato.Effects.Flanger({
+    time: 0.8,
+    depth: 0.2,
+    speed: 0.2,
+    feedback: 0.5,
+    mix: 1
+  })
+
+  synthReverb = new Pizzicato.Effects.Reverb({
+    time: 1.6,
+    decay: 1.6,
+    reverse: true,
+    mix: 0.5
+  });
+
+  // background synth
+  synthB = new Pizzicato.Sound({
+    source: 'wave',
+  });
+  // background synth effects
+  synthBdelay = new Pizzicato.Effects.PingPongDelay({
+    feedback: 0.5,
+    time: 0.2,
+    mix: 0.5
+  });
+
+  // applying effects
+  drums.addEffect(drumDist);
+  drums.addEffect(drumLPF);
+  snare.addEffect(snareReverb);
+  bass.addEffect(bassDist);
+  synth.addEffect(synthLPF);
+  synth.addEffect(synthFlanger);  synth.addEffect(synthReverb);
+  synthB.addEffect(synthBdelay);
 }
 
 function draw() {
@@ -69,27 +147,31 @@ function mousePressed() {
   if (!playing){
     setInterval(playBass,200);
     setTimeout(playNote,200);
+    setInterval(playNoteB,100);
     setInterval(playDrum,200);
+    setInterval(lowPassMod,5);
     playing = true;
-    setInterval(reverbMix,400);
   }
 }
 
-function reverbMix() {
-
-  if (reverbModUp === true) {
-    reverbMod = reverbMod + 0.1;
+function lowPassMod() {
+  synthLPF.frequency = lowpassLFO;
+  console.log(lowpassLFO);
+  if (lowpassLFO >= 4000) {
+    lowpassLFOup = false;
   }
-  if (reverbModUp === false) {
-    reverbMod = reverbMod - 0.1;
+  else if (lowpassLFO <= 400) {
+    lowpassLFOup = true;
   }
-
-
-  if (reverbMod >= 1) {
-    reverbModUp = false;
+  if (lowpassLFOup){
+    lowpassLFO = lowpassLFO + 4;
+  }
+  else if (!lowpassLFOup){
+    lowpassLFO = lowpassLFO - 4;
   }
 }
 
+// randomly plays a bass note and its upper octave neighbour
 function playBass() {
   let bassFreq = bassFreqs[floor(random() * bassFreqs.length)];
   bass.frequency = bassFreq;
@@ -98,13 +180,14 @@ function playBass() {
 
 function playNote() {
 
-  noteRNG = Math.random();
+  restRNG = Math.random();
+  lengthRNG = Math.random();
 
   let synthFreq = synthFreqs[floor(random() * synthFreqs.length)];
   synth.frequency = synthFreq;
+  synth.volume = 0.2;
 
-  if (noteRNG <= 0.7) {
-
+  if (restRNG <= 0.7) {
     synth.play();
   }
   else {
@@ -113,7 +196,7 @@ function playNote() {
     return;
   }
 
-  if (noteRNG > 0.7) {
+  if (lengthRNG > 0.7) {
     setTimeout(playNote,400);
   }
   else {
@@ -121,7 +204,18 @@ function playNote() {
   }
 }
 
+function playNoteB() {
+  synthB.volume = 0.2;
+  let synthFreqB = synthFreqsB[floor(random() * synthFreqsB.length)];
+  synthB.frequency = synthFreqB;
+  synthB.play();
+}
+
 function playDrum() {
+  kick.stop();
+  snare.stop();
+  hat.stop();
+
   let symbols = pattern[patternIndex];
   if (symbols.indexOf('k') !== -1) {
     kick.play();
