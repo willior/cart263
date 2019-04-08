a0d677e8ba3ba31844bc5b229f31ae1ee95db15d
willjeez <willgs@gmail.com>
2019-04-08 19:58:36 -0400

    learning to create a top-down 2D adventure game using phaser

---
+++ projects/project3/src/config.js
@@ -0,0 +1,12 @@
+import 'phaser';
+
+export default {
+  type: Phaser.AUTO,
+  parent: "phaser-example",
+  width: 800,
+  height: 600,
+  scene: {
+    preload: preload,
+    create: create
+  }
+};
