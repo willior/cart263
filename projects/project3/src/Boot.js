8d1303ad1acaedddff1ec9c695024816ea63edd3
willjeez <willgs@gmail.com>
2019-04-12 01:12:35 -0400

    working on title screen code

--- projects/project3/src/Boot.js
+++ projects/project3/src/Boot.js
@@ -1,21 +1,32 @@
 import 'phaser';
 
 export default class BootScene extends Phaser.Scene {
-  constructor (key) {
-    super(key);
-  }
-
-  preload () {
+  constructor () {
+    super({key: 'BootScene'});
     this.maps = {
-      1: 'map1',
-      2: 'map2',
-      3: 'map3'
-    };
+    title: {
+      key: "TitleScreen",
+      path: "assets/maps/title_screen.json"
+    }
+  };
+}
+
+preload () {
+      for (let map_name in this.maps) {
+        let map = this.maps[map_name];
+        this.load.json(map_name, map.path);
+      }
+
+
+      // 1: 'map1',
+      // 2: 'map2',
+      // 3: 'map3'
+    
 
     // loading tilemap
-    this.load.tilemapTiledJSON('map1', 'assets/maps/map1.json');
-    this.load.tilemapTiledJSON('map2', 'assets/maps/map2.json');
-    this.load.tilemapTiledJSON('map3', 'assets/maps/map3.json');
+    // this.load.tilemapTiledJSON('map1', 'assets/maps/map1.json');
+    // this.load.tilemapTiledJSON('map2', 'assets/maps/map2.json');
+    // this.load.tilemapTiledJSON('map3', 'assets/maps/map3.json');
 
     // loading spritesheet
     this.load.spritesheet('masterTileset', 'assets/images/masterTileset.png', { frameWidth: 16, frameHeight: 16});
@@ -27,6 +38,11 @@ export default class BootScene extends Phaser.Scene {
   }
 
   create () {
-    this.scene.start('Game', { map: 1, newGame: true, maps: this.maps });
+    let map_data = this.cache.json.get('title');
+    console.log(tilemapTiledJSON_data);
+
+
+
+    // this.scene.start('Game', { map: 1, newGame: true, maps: this.maps });
   }
 };
--- projects/project3/src/index.js
+++ projects/project3/src/index.js
@@ -1,12 +1,17 @@
 import Phaser from "phaser";
 import config from './config';
+import TitleScene from './TitleScene';
 import GameScene from './Game';
 import BootScene from './Boot';
 // import UIPlugin from './ui/ui-plugin.js'
 
+let titleScene = new TitleScene();
+let bootScene = new BootScene();
+
 class Game extends Phaser.Game {
   constructor () {
     super(config);
+    this.scene.add('TitleScene', titleScene);
     this.scene.add('Boot', BootScene);
     this.scene.add('Game', GameScene);
     this.scene.start('Boot');
