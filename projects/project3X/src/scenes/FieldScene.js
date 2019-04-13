6139caeb6c016b484b2d4b7094f3a87f4a93387e
willjeez <willgs@gmail.com>
2019-04-12 21:16:06 -0400

    field screen updated to iterate through properties defined in Tiled
    iterates through tilemaps, then through layers (adding collision when applicable),  then objects, then creates the object through prefabs. not sure if it will work

--- projects/project3X/src/scenes/FieldScene.js
+++ projects/project3X/src/scenes/FieldScene.js
@@ -10,6 +10,35 @@ class FieldScene extends JSONLevelScene {
       player: Prefab.prototype.constructor
     }
   }
+
+  create() {
+    this.screen = this.add.tilemap(this.map_data.screen.key);
+
+    let tileset_index = 0;
+    this.tilesets = {};
+    this.screen.tilesets.forEach(function (tileset) {
+      let screen_tileset = this.screen.addTilesetImage(tileset.name, this.map_data.screen.tilesets[tileset_index]);
+      this.tilesets[this.map_data.screen.tilesets[tileset_index]] = screen_tileset;
+      tileset_index += 1;
+    }, this);
+
+    this.layers = {};
+    this.screen.layers.forEach(function (layer) {
+      this.layers[layer.name] = this.screen.createStaticLayer(layer.name, this.tilesets[layer.properties.tileset]);
+      if (layer.properties.collision) {
+        this.screen.setCollisionByExclusion([-1], true, layer.name);
+      }
+    }, this);
+    super.create();
+    this.screen.objects.forEach(function (object_layer) {
+      object_layer.objects.forEach(this.create_object, this);
+    }, this);
+  }
+
+  create_object (object) {
+    
+  }
+
 }
 
 export default FieldScene;
