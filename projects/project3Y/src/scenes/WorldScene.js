040f800f5f781eeb621d1de3dd9d040ac4fab5dc
willjeez <willgs@gmail.com>
2019-04-13 14:57:12 -0400

    i have pinpointed the error to be the tilemap JSON file exported from Tiled, as when i use another tilemap & tileset, the game loads, but not with mine, despite all parameters being identical.

--- projects/project3Y/src/scenes/WorldScene.js
+++ projects/project3Y/src/scenes/WorldScene.js
@@ -12,35 +12,29 @@ class WorldScene extends JSONLevelScene {
   }
 
   create() {
-    console.log("world scene creation");
     this.map = this.add.tilemap(this.level_data.map.key);
     let tileset_index = 0;
     this.tilesets = {};
     this.map.tilesets.forEach(function (tileset) {
-      let map_tileset = this.map.addTilesetImage(tileset.name, this.level_data.map.tilesets[tileset_index]);
-      this.tilesets[this.level_data.map.tilesets[tileset_index]] = map_tileset;
-      tileset_index += 1;
+        let map_tileset = this.map.addTilesetImage(tileset.name, this.level_data.map.tilesets[tileset_index]);
+        this.tilesets[this.level_data.map.tilesets[tileset_index]] = map_tileset;
+        tileset_index += 1;
     }, this);
-    console.log("tilesets added");
+
     this.layers = {};
     this.map.layers.forEach(function (layer) {
-      console.log("adding layers");
-      this.layers[layer.name] = this.map.createStaticLayer(layer.name, this.tilesets[layer.properties.tileset]);
-      console.log("creating layers");
-      if (layer.properties.collision) {
-        console.log("adding collision");
-        this.map.setCollisionByExclusion([-1], true, layer.name);
-      }
+        this.layers[layer.name] = this.map.createStaticLayer(layer.name, this.tilesets[layer.properties.tileset]);
+        if (layer.properties.collision) { // collision layer
+            this.map.setCollisionByExclusion([-1], true, layer.name);
+        }
     }, this);
-    console.log("collision added");
 
     super.create();
 
     this.map.objects.forEach(function (object_layer) {
-
-      object_layer.objects.forEach(this.create_object, this);
+        object_layer.objects.forEach(this.create_object, this);
     }, this);
-  }
+    }
 
   create_object (object) {
     let position = {x: object.x + (object.width / 2), y: object.y + (object.height / 2)};
