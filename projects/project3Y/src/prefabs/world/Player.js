4a63d68222ff936a0f9bf6eb8a31f0157680d39b
willjeez <willgs@gmail.com>
2019-04-14 19:35:39 -0400

    movement doesn't want to work.

--- projects/project3Y/src/prefabs/world/Player.js
+++ projects/project3Y/src/prefabs/world/Player.js
@@ -15,30 +15,89 @@ class Player extends Prefab {
     this.move_up = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
     this.move_down = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
 
+    if (!this.scene.anims.anims.has('walking_down')) {
+      this.scene.anims.create({
+        key: 'walking_down',
+        frames: this.scene.anims.generateFrameNumbers(this.texture.key, {frames: [13, 14, 15, 16, 17, 18]}),
+        frameRate: 8,
+        repeat: -1
+      });
+    }
+
+    if (!this.scene.anims.anims.has('walking_up')) {
+      this.scene.anims.create({
+        key: 'walking_up',
+        frames: this.scene.anims.generateFrameNumbers(this.texture.key, {frames: [18, 17, 16, 15, 14, 13]}),
+        frameRate: 8,
+        repeat: -1
+      });
+    }
+
+    if (!this.scene.anims.anims.has('walking_right')) {
+      this.scene.anims.create({
+        key: 'walking_right',
+        frames: this.scene.anims.generateFrameNumbers(this.texture.key, {frames: [7, 8, 9, 10, 11, 12]}),
+        frameRate: 8,
+        repeat: -1
+      });
+    }
+
+    if (!this.scene.anims.anims.has('walking_left')) {
+      this.scene.anims.create({
+        key: 'walking_left',
+        frames: this.scene.anims.generateFrameNumbers(this.texture.key, {frames: [13, 14, 15, 16, 17, 18]}),
+        frameRate: 8,
+        repeat: -1
+      });
+    }
+    this.stopped_frames = [0, 0, 0, 1, 7]
   }
 
   update() {
+
+
     if (this.move_left.isDown && this.body.velocity.x <= 0) {
       console.log("moving left");
       this.body.velocity.x = -this.walking_speed;
+      if (this.body.velocity.y === 0) {
+        this.anims.play('walking_left', true);
+      }
     }
     else if (this.move_right.isDown && this.body.velocity.x >= 0) {
       console.log("moving right");
       this.body.velocity.x = this.walking_speed;
+      if (this.body.velocity.y === 0) {
+        this.anims.play('walking_right', true);
+      }
     }
     else {
       this.body.velocity.x = 0
     }
     if (this.move_up.isDown && this.body.velocity.y <= 0) {
+      if (cursors.up.isDown) {
+        console.log("up");
+      }
+
+
       console.log("moving up");
       this.body.velocity.y = -this.walking_speed;
+      if (this.body.velocity.x === 0) {
+        this.anims.play('walking_up', true);
+      }
     }
     else if (this.move_down.isDown && this.body.velocity.y >= 0) {
       console.log("moving down");
       this.body.velocity.y = this.walking_speed;
+      if (this.body.velocity.x === 0) {
+        this.anims.play('walking_down', true);
+      }
     }
     else {
       this.body.velocity.y = 0
+    }
+    if (this.body.velocity.x === 0 && this.body.velocity.y === 0) {
+      this.anims.stop();
+      this.setFrame(this.stopped_frames[this.body.facing]);
     }
   }
 }
