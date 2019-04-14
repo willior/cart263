973e006ab3b1cba8b5224b5f9bf40c2d70047f89
willjeez <willgs@gmail.com>
2019-04-14 17:43:45 -0400

    updated files; started player Prefab

---
+++ projects/project3Y/src/prefabs/world/Player.js
@@ -0,0 +1,11 @@
+import Prefab from '../Prefab';
+
+class Player extends Prefab {
+  constructor(scene, name, position, properties) {
+    super(scene, name, position, properties);
+
+    this.walking_speed = +properties.walking_speed;
+  }
+}
+
+export default Player;
