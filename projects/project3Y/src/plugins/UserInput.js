eb3c2dffce52fd6c47f7f35ffb2841fbacfe9278
willjeez <willgs@gmail.com>
2019-04-14 21:37:44 -0400

    working on configurable controls through the creation of a User Input plugin

--- projects/project3Y/src/plugins/UserInput.js
+++ projects/project3Y/src/plugins/UserInput.js
@@ -3,6 +3,22 @@ class UserInput {
     this.scene = scene;
     this.enabled = false;
   }
+  set_input (user_input_data) {
+    this.scene.input.keyboard.removeAllListeners('keydown');
+    this.scene.input.keyboard.removeAllListeners('keyup');
+
+    this.scene.input.keyboard.on('keydown', this.process_input, this);
+    this.scene.input.keyboard.on('keyup'), this.process_input, this);
+
+    this.user_inputs = user_input_data;
+
+    this.enabled = true;
+  }
+
+  process_input (event) {
+    
+  }
+
 }
 
 export default UserInput;
