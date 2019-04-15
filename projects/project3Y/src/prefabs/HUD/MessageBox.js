1a6da198238130946e0806efef918eeaebd12440
willjeez <willgs@gmail.com>
2019-04-15 15:12:51 -0400

    started a simple message box prefab.
    going to make a very rudimentary message box first, then attempt the more complicated stuff later on.

---
+++ projects/project3Y/src/prefabs/HUD/MessageBox.js
@@ -0,0 +1,9 @@
+import Prefab from '../Prefab';
+
+class MessageBox extends Prefab {
+  constructor(scene, name, position, properties) {
+    super(scene, name, position, properties);
+  }
+}
+
+export default MessageBox;
