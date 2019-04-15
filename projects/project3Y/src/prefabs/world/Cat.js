ed72f59ef0d08542aa2629d7ccba80dd6ece588c
willjeez <willgs@gmail.com>
2019-04-15 14:52:31 -0400

    cat tail wag achieved! he looks so cute. will edit the tail animation. might give the tail wag animation a ridiculous amount of frames if i feel like it.

--- projects/project3Y/src/prefabs/world/Cat.js
+++ projects/project3Y/src/prefabs/world/Cat.js
@@ -8,13 +8,27 @@ class Cat extends Prefab {
 
     this.body.immovable = true;
 
-    this.scene.physics.add.collider(this, this.scene.groups.players,
-       this.talk, null, this);
+    this.scene.physics.add.collider(this, this.scene.groups.players, this.talk, null, this);
+
+    if (!this.scene.anims.anims.has('cat_tail')) {
+      this.scene.anims.create({
+        key: 'cat_tail',
+        frames: this.scene.anims.generateFrameNumbers(this.texture.key, {frames: [0, 1, 2, 3, 4, 5, 6, 6]}),
+        frameRate: 8,
+        repeat: -1
+      });
+    }
   }
 
   talk(cat, player) {
     player.stop();
     console.log(this.message);
+  }
+
+  update() {
+    if (this.body) {
+      this.anims.play('cat_tail', true);
+    }
   }
 }
 
