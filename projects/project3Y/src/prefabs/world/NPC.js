92ca78fc1700315f095f0c70db676b57c4c1164e
willjeez <willgs@gmail.com>
2019-04-16 13:49:15 -0400

    updated map 8, updated spritesheets...
    this is a lot of tedious work, but i'm learning a lot about how to and how not to properly structure a project like this, at least

--- projects/project3Y/src/prefabs/world/NPC.js
+++ projects/project3Y/src/prefabs/world/NPC.js
@@ -13,6 +13,15 @@ class NPC extends Prefab {
 
     this.scene.physics.add.collider(this, this.scene.groups.players,
     this.talk, null, this);
+
+    if (!this.scene.anims.anims.has('animation')) {
+      this.scene.anims.create({
+        key: 'animation',
+        frames: this.scene.anims.generateFrameNumbers(this.texture.key, {frames: [0, 1, 2, 3, 4, 5, 6, 7]}),
+        frameRate: 8,
+        repeat: -1
+      });
+    }
   }
 
   talk(npc, player) {
@@ -20,7 +29,11 @@ class NPC extends Prefab {
     player.stop();
     this.scene.current_message_box = new MessageBox(this.scene, this.name + '_message_box', this.MESSAGE_BOX_POSITION, {texture: 'message_box_image', group: 'hud', message: this.message});
     this.scene.user_input.set_input(this.scene.user_inputs.talking_user_input);
-    console.log("talking to npc");
+  }
+  update() {
+    if (this.body) {
+      this.anims.play('animation', true);
+    }
   }
 }
 
