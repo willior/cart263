d57611f8b98cf0fb5e89e2d2d68734bfef4af0da
willjeez <willgs@gmail.com>
2019-04-19 21:20:56 -0400

    cannot get typewriter effect to work, which is weird because i had it working yesterday.
    super fffffffffrusting because i am attempting to instantiate the most basic text possible by literally copypasting the "basic text example" on the phaser 3 examples website. it gives me an error. doesn't even get to the typewriter part. just gives me fucking errors that make no sense.
    i remember getting it to work yesterday by adding an extra 'this.' somewhere but i haven't succeeded yet. i might just move on from this typewriter thing because i need to finish the game.

--- projects/project3/src/prefabs/world/NPC.js
+++ projects/project3/src/prefabs/world/NPC.js
@@ -18,15 +18,41 @@ class NPC extends Prefab {
   talk(npc, player) {
     player.stop();
 
-    this.MESSAGE_BOX_POSITION = {x: (player.x-280), y: (player.y+20)};
-    this.scene.current_message_box = new MessageBox(this.scene, this.name + '_message_box', this.MESSAGE_BOX_POSITION, {texture: 'message_box_image', group: 'hud', message: this.message});
+    if (npc.name = 'tree') {
 
-    // stops listening for key presses
-    this.scene.input.keyboard.removeAllListeners('keydown');
-    this.scene.input.keyboard.removeAllListeners('keyup');
-    // for one second
-    var closeTime = this.scene.time.delayedCall(1000, this.closable, [], this);
-    console.log('talking to NPC; cannot close textbox for 1 second');
+
+      // let titleStyle = { fontFamily: 'Commodore', fontSize: '38px', fill: '#aadddd', wordWrap: true, align: 'left' };
+
+
+      this.add.text(100,100,'hi');
+
+      this.textHolder = "I know what you're thinking.";
+
+      let index = 0;
+      let interval = setInterval(() => {
+        textStart.textHolder += textHolder.charAt(index);
+        index++;
+        if (index === textHolder.length) {
+          clearInterval(interval);
+        }
+      },50);
+
+
+    }
+    else {
+
+      this.MESSAGE_BOX_POSITION = {x: (player.x-280), y: (player.y+20)};
+      this.scene.current_message_box = new MessageBox(this.scene, this.name + '_message_box', this.MESSAGE_BOX_POSITION, {texture: 'message_box_image', group: 'hud', message: this.message});
+
+      // stops listening for key presses
+      this.scene.input.keyboard.removeAllListeners('keydown');
+      this.scene.input.keyboard.removeAllListeners('keyup');
+      // for one second
+      var closeTime = this.scene.time.delayedCall(1000, this.closable, [], this);
+      console.log('talking to NPC; cannot close textbox for 1 second');
+  }
+
+
   }
   closable(){
     // after 1 second, reapplies inputs so the text box can be closed
